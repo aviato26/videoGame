dfee23028c01facdd8e5c560c7d888e6fdafb02e
shaun deshler <shaun@shauns-iMac.local>
2019-06-10 20:01:11 -0700

    adding gulp and browser-sync

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+let gulp = require('gulp');
+let browserSync = require('browser-sync');
+
+gulp.task('sync', function() {
+  browserSync.init(['./*.css', './*.html', './*.js'], {
+    server: {
+      baseDir: "./"
+    }
+  });
+});
